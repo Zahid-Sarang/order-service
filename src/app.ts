875a46e87066b35228e55cdc534b27cd43f0e1d2
zahid-soft <zahidsarang95@gmail.com>
2024-07-04 08:00:47 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
 import express, { Request, Response } from "express";
-import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
+import cors from "cors";
+import config from "config";
 import cookieParser from "cookie-parser";
+
+import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import customerRouter from "./customer/customerRouter";
 import couponRoute from "./coupon/couponRoute";
 import orderRouter from "./order/orderRouter";
 import paymentRouter from "./payment/paymentRouter";
+
 const app = express();
+
+const ALLOWED_DOMAINS = [
+  config.get<string>("frontend.clientUI"),
+  config.get<string>("frontend.adminUI"),
+];
+
+app.use(
+  cors({
+    origin: ALLOWED_DOMAINS,
+  }),
+);
+
+console.log(ALLOWED_DOMAINS);
+
 app.use(cookieParser());
 app.use(express.json());
 
