a45e5fb3e09d2c2bedd03622c92ced97b3e46593
zahid-soft <zahidsarang95@gmail.com>
2024-07-05 11:23:53 +0530

    add sasl kafka authentication

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -1,4 +1,10 @@
-import { Consumer, EachMessagePayload, Kafka, Producer } from "kafkajs";
+import {
+  Consumer,
+  EachMessagePayload,
+  Kafka,
+  KafkaConfig,
+  Producer,
+} from "kafkajs";
 import config from "config";
 import { MessageBroker } from "../types/broker";
 import { handleProductUpdate } from "../productCache/productUpdateHandler";
@@ -9,7 +15,25 @@ export class KafkaBroker implements MessageBroker {
   private producer: Producer;
 
   constructor(clientId: string, brokers: string[]) {
-    const kafka = new Kafka({ clientId, brokers });
+    let kafkaConfig: KafkaConfig = {
+      clientId,
+      brokers,
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      kafkaConfig = {
+        ...kafkaConfig,
+        ssl: true,
+        connectionTimeout: 45000,
+        sasl: {
+          mechanism: "plain",
+          username: config.get("kafka.sasl.username"),
+          password: config.get("kafka.sasl.password"),
+        },
+      };
+    }
+
+    const kafka = new Kafka(kafkaConfig);
 
     this.producer = kafka.producer();
     this.consumer = kafka.consumer({ groupId: clientId });
